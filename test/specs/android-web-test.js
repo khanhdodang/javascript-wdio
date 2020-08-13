3a287c0d3ff6e22def2811c914065d8428677ee1
Khanh Do <khanhdo.pmp@gmail.com>
2020-08-13 16:24:15 +0700

    Change to the new URL

--- test/specs/android-web-test.js
+++ test/specs/android-web-test.js
@@ -1,15 +1,15 @@
 describe('WebdriverIO', () => {
   it('should access to Shadown DOM', async () => {
-    const allBooks = '#allBooks'
-    const aStoreLink = 'a[href*="product"]'
-    
-    await browser.url('http://www.kaidez.com/samples/template-shadowdom-practice/')
+    await browser.url('https://material-components.github.io/material-components-web-components/demos/switch.html')
     await browser.pause(3000)
 
-    const innerEl = await $(allBooks)
-    await innerEl.shadow$$(aStoreLink)
-    await innerEl.shadow$(aStoreLink)
-
-    console.log(await innerEl.getValue())
+    element = await browser.$('//mwc-switch[1]')
+    let innerEl = await element.shadow$$('#basic-switch')
+    // Perform click action on shadow DOM
+    await innerEl[0].click()
+    
+    element = await browser.$('//mwc-switch[1]')
+    innerEl = await element.shadow$$('#basic-switch')
+    await innerEl[0].click()
   })
 })
